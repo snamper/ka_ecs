031d59c103f24d7787462bcf60856e6c9b1f5772
thinkmix <thinkmix@163.com>
2018-06-08 00:01:44 +0800

    修复商户详情售卡范围返回值格式；ym

--- src/componentskm/merchantDetails.vue
+++ src/componentskm/merchantDetails.vue
@@ -346,7 +346,7 @@ export default {
             const vm=this.$parent;
 
             let imgUrl=_CONFIG ? _CONFIG[_CONFIG.env].REGISTER_MERCHANT_IMAGE_URL : '';
-            let merchantType=vm._ajaxData.details.merchantType;
+            let merchantType=JSON.parse(vm._ajaxData.details.merchantType);
             vm.merchantImgData=[
                 {'src':vm._ajaxData.details.frontImageUrl?imgUrl+vm._ajaxData.details.frontImageUrl:'','name':merchantType=='企业' ? '门店左照片' : '正面照片'},
                 {'src':vm._ajaxData.details.backImageUrl?imgUrl+vm._ajaxData.details.backImageUrl:'','name':merchantType=='企业' ? '门店右照片' : '反面照片'},
