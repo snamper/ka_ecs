d7885dbab53886928a41a9a9b2eb46f8881ff84b
Your Name <thinkmix@163.com>
2018-06-27 15:40:09 +0800

    修改图片路径配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       // repo : 'git@github.com:thinkmix/ka_ecs.git',
       repo : 'https://github.com/thinkmix/ka_ecs.git',
       path : '/home/nginx/html/ka_ecs',
-      'post-deploy' : 'cnpm install && npm run build && pm2 reload ecosystem.config.js --env test'
+      'post-deploy' : 'cnpm install && npm run buildTest && pm2 reload ecosystem.config.js --env test'
     },
     onlineTest : {
       user : 'root',
