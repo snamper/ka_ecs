17de67979a7473220d215f9f84ca2b700f5b0da4
Your Name <thinkmix@163.com>
2018-08-16 22:07:52 +0800

    订单审核i卡sdk区分

--- src/componentskm/makeCardDetails.vue
+++ src/componentskm/makeCardDetails.vue
@@ -72,8 +72,7 @@
                             <tr>
                                 <td>MAC地址：</td>
                                 <td>{{detailsEmpty.mac||'--'}}</td>
-                                <td>短信校验号码：</td>
-                                <td>{{detailsEmpty.safe_phone}}</td>
+                                
                             </tr>
                         </tbody>
                         <!-- 白卡 -->
@@ -194,7 +193,7 @@
                                     <td>{{translateData('money',detailsEmpty.prestore_money)||'--'}}元</td>
                                     <td>短信验证：</td>
                                     <td>
-                                        <span v-if="detailsEmpty.safe_type==1">是</span>
+                                        <span v-if="detailsEmpty.safe_type==1">是（{{detailsEmpty.safe_phone}}）</span>
                                         <span v-else-if="detailsEmpty.safe_type==0">否</span>
                                         <span v-else>--</span>
                                     </td>
