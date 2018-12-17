201f6837f032563e8a3b920bacfafd2fe15b50bf
tengbo <thinkmix@163.com>
2018-12-17 10:08:06 +0800

    tb;脚手架代理设置https模式，i卡审核新增iccid展现

--- src/views/km/audit/cardItem.vue
+++ src/views/km/audit/cardItem.vue
@@ -68,6 +68,10 @@
                       <td>归属地：</td>
                       <td>{{ auditData.cityName||'--'}}</td>
                     </tr>
+                    <tr v-show="off.itemType==8||off.itemType==9">
+                      <td>ICCID：</td>
+                      <td>{{ auditData.iccid ||'--'}}</td>
+                    </tr>
                     <tr v-show="off.itemType!=8&&off.itemType!=9">
                       <td>IMEI：</td>
                       <td>{{ auditData.IMEI ||'--'}}</td>
